349d17c7d023775f89b344e4d9ff62fc19c1cbbd
spm <majining@users.noreply.github.com>
2020-11-16 13:42:42 -0600

    basic page initialized

---
+++ health-data-app/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Sample Health Data Frontend with Angular';
+
+  public constructor(private titleService: Title) {}
+
+  public setTitle( newTitle: string) {
+    this.titleService.setTitle( newTitle );
+  }
+}
